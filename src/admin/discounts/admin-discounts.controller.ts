d33369eefb2eaf5bd9364d664f40e78acb8c2378
Daniel Oto Diaz <danieloto212@gmail.com>
2025-09-16 02:50:15 -0300

    update swager ej admin-discounts

--- src/admin/discounts/admin-discounts.controller.ts
+++ src/admin/discounts/admin-discounts.controller.ts
@@ -4,18 +4,32 @@ import { CreateDiscountDto } from "./dto/create-discount.dto";
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 import { RolesGuard } from "../../auth/roles/roles.guard";
 import { Roles } from "../../auth/roles/roles.decorator";
-import { ApiBearerAuth, ApiTags, ApiOperation } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags, ApiOperation, ApiBody } from "@nestjs/swagger";
 
 @ApiTags("Admin Discounts")
 @Controller("admin/discounts")
 @UseGuards(JwtAuthGuard, RolesGuard)
+@ApiBearerAuth("access-token")
 export class AdminDiscountsController {
   constructor(private readonly discountsService: AdminDiscountsService) {}
 
   @Post()
   @Roles("employee")
-  @ApiBearerAuth("access-token")
   @ApiOperation({ summary: "Creá un descuento para cliente" })
+  @ApiBody({
+    type: CreateDiscountDto,
+    examples: {
+      example1: {
+        summary: "Descuento ejemplo",
+        value: {
+          userId: "11111111-2222-3333-4444-555555555555",
+          percentage: 15,
+          validTo: "2025-12-31",
+          active: true
+        }
+      }
+    }
+  })
   create(@Body() dto: CreateDiscountDto) {
     return this.discountsService.create(dto);
   }
